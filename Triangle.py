3b05c8ce41292acbe82edd4c5c7a0a940a313e12
Omar <oelkhafi@stevens.edu>
2025-03-12 02:00:50 -0400

    improving triangle identification method

--- Triangle.py
+++ Triangle.py
@@ -1,15 +1,19 @@
 def classify_triangle(a,b,c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return "Not a valid triangle"
     sides = sorted([a, b, c])
     a, b, c = sides
-    if a==b and a==c and b==c:
+    if a + b <= c:
+        return "Not a valid triangle"
+    if a == b == c:
         return "equilateral"
+    epsilon = 1e-7
+    is_right = abs(a**2 + b**2 - c**2) < epsilon
+
+    if a == b or b == c or a == c:
+        triangle_type = "isosceles"
     else:
-        is_right = a**2 + b**2 == c**2
-        triangle_type = ""
-
-        if a == b or b == c or a == c:
-            triangle_type = "isosceles"
-        else:
-            triangle_type = "scalene"
-
-        return f"{triangle_type} right triangle" if is_right else triangle_type
+        triangle_type = "scalene"
+    if is_right:
+        triangle_type += " right triangle"
+    return triangle_type
